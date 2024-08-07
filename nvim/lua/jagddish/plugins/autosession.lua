a9b9a78726df4dac8e304bc4ebe3d59bf912124d
Jagddish <jagddish2@gmail.com>
2024-08-07 12:12:46 +0530

    added keymap

--- nvim/lua/jagddish/plugins/autosession.lua
+++ nvim/lua/jagddish/plugins/autosession.lua
@@ -1,30 +1,28 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    local auto_session = require("auto-session")
+	"rmagatti/auto-session",
+	lazy = false,
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
+	keys = {
+		-- Will use Telescope if installed or a vim.ui.select picker otherwise
+		{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+		{ "<leader>wr", "<cmd>SessionRestore<CR>", desc = "Restore session" },
 
-    auto_session.setup({
-      post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-        require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-      end,
-      auto_restore_enabled = false,
-      auto_session_enable_last_session = true,
-      auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
-      session_lens = {
-        -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-        buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-        load_on_setup = true,
-        theme_conf = { border = true },
-        previewer = false,
-      },
-      vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
-        noremap = true,
-      }),
-    })
-
-    local keymap = vim.keymap
-
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
-  end,
+		{ "<C-s>", "<cmd>SessionSearch<CR>", desc = "Session search" },
+	},
+	config = function()
+		require("auto-session").setup({
+			auto_restore_enabled = false,
+			auto_session_enable_last_session = false,
+			auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+			session_lens = {
+				-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+				buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+				load_on_setup = true,
+				theme_conf = { border = true },
+				previewer = false,
+			},
+		})
+	end,
 }
