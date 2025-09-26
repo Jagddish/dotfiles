9773f9461fb63da924ea1101cb3be668e794a745
Jagddish <jagddish2@gmail.com>
2025-09-26 21:53:42 +0530

    commented ts lsp keymaps

--- nvim/lua/plugins/lsp/typescript.lua
+++ nvim/lua/plugins/lsp/typescript.lua
@@ -35,31 +35,31 @@ return {
     -- TypeScript LSP Keymaps
     -- =========================
     -- Map keys buffer-locally when LSP attaches
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(event)
-        local buf = event.buf
-        local function map(keys, func, desc)
-          vim.keymap.set("n", keys, func, { buffer = buf, desc = "TS: " .. desc })
-        end
+    -- vim.api.nvim_create_autocmd("LspAttach", {
+    --   callback = function(event)
+    --     local buf = event.buf
+    --     local function map(keys, func, desc)
+    --       vim.keymap.set("n", keys, func, { buffer = buf, desc = "TS: " .. desc })
+    --     end
 
-        -- Rename symbol
-        map("<leader>tr", ts_tools.rename, "Rename Symbol")
+    --     -- Rename symbol
+    --     map("<leader>tr", ts_tools.rename, "Rename Symbol")
 
-        -- Organize imports
-        map("<leader>to", ts_tools.organize_imports, "Organize Imports")
+    --     -- Organize imports
+    --     map("<leader>to", ts_tools.organize_imports, "Organize Imports")
 
-        -- Add missing imports
-        map("<leader>ta", ts_tools.add_missing_imports, "Add Missing Imports")
+    --     -- Add missing imports
+    --     map("<leader>ta", ts_tools.add_missing_imports, "Add Missing Imports")
 
-        -- Remove unused code
-        map("<leader>tu", ts_tools.remove_unused, "Remove Unused Code")
+    --     -- Remove unused code
+    --     map("<leader>tu", ts_tools.remove_unused, "Remove Unused Code")
 
-        -- Fix all auto-fixable issues
-        map("<leader>tf", ts_tools.fix_all, "Fix All")
+    --     -- Fix all auto-fixable issues
+    --     map("<leader>tf", ts_tools.fix_all, "Fix All")
 
-        -- Go to source definition (works with type-only imports)
-        map("<leader>td", ts_tools.go_to_source_definition, "Go To Source Definition")
-      end,
-    })
+    --     -- Go to source definition (works with type-only imports)
+    --     map("<leader>td", ts_tools.go_to_source_definition, "Go To Source Definition")
+    --   end,
+    -- })
   end,
 }
